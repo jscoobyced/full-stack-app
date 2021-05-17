720e884b36760ed210b32b74aeb827fff6401559
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-17 21:01:40 +0700

    Fix DB script and image

--- .github/db-update.sh
+++ .github/db-update.sh
@@ -12,10 +12,10 @@ DBUSER=${3}
 DBPASS=${4}
 DBNAME=${5}
 
-docker -H "ssh://${1}@${2}" exec docker-nodejs-mariadb_db_1 sh -c \
+docker -H "ssh://${1}@${2}" exec full-stack-app_db_1 sh -c \
   "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${DOCKER_UPDATE_FOLDER}/update.sql"
 
-docker -H "ssh://${1}@${2}" exec docker-nodejs-mariadb_db_1 sh -c \
+docker -H "ssh://${1}@${2}" exec full-stack-app_db_1 sh -c \
   "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${DOCKER_UPDATE_FOLDER}/stored_procedures.sql"
 
 rm -Rf "${HOME}/.ssh"
