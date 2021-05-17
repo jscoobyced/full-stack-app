54ee420f84e3bf26f1ec75a7bb885822e46c4a6c
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-17 21:28:28 +0700

    Fixing deploy scripts.

--- .github/db-update.sh
+++ .github/db-update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Preparing configuration"
+
 DTFMT=$(date +%Y%m%d-%H%M%S)
 UPDATE_FOLDER="~/db/updates/${DTFMT}"
 DOCKER_UPDATE_FOLDER="/home/${DTFMT}"
@@ -8,14 +10,19 @@ scp ./db/sql/update/update.sql "$1@$2:${UPDATE_FOLDER}/"
 cat ./db/sql/update/stored_procedure/*.sql > stored_procedures.sql
 scp stored_procedures.sql "$1@$2:${UPDATE_FOLDER}/"
 
-DBUSER=${3}
-DBPASS=${4}
-DBNAME=${5}
+echo "Setting DB parameters"
+DBUSER="${3}"
+DBPASS="${4}"
+DBNAME="${5}"
 
-docker -H "ssh://${1}@${2}" exec full-stack-app_db_1 sh -c \
-  "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${DOCKER_UPDATE_FOLDER}/update.sql"
+echo "Configuration complete"
 
+echo "Updating schema"
 docker -H "ssh://${1}@${2}" exec full-stack-app_db_1 sh -c \
-  "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${DOCKER_UPDATE_FOLDER}/stored_procedures.sql"
+  "mysql --host=localhost --port=3306 --protocol=tcp -u \"${DBUSER}\" -p\"${DBPASS}\" \"${DBNAME}\" < ${DOCKER_UPDATE_FOLDER}/update.sql"
+
+echo "Adding stored procedures"
+docker -H "ssh://${1}@${2}" exec full-stack-app_db_1 sh -c \
+  "mysql --host=localhost --port=3306 --protocol=tcp -u \"${DBUSER}\" -p\"${DBPASS}\" \"${DBNAME}\" < ${DOCKER_UPDATE_FOLDER}/stored_procedures.sql"
 
 rm -Rf "${HOME}/.ssh"
--- db/sql/update/stored_procedure/get_categories_v1.sql
+++ db/sql/update/stored_procedure/get_categories_v1.sql
@@ -1,17 +1,19 @@
 DROP PROCEDURE IF EXISTS get_categories_v1;
 
-DELIMITER / / CREATE PROCEDURE get_categories_v1 (IN parameter_active BOOLEAN) BEGIN
-SELECT
-  id,
-  `name`,
-  created,
-  updated,
-  active
-FROM
-  category c
-WHERE
-  c.active = parameter_active;
+DELIMITER //
+CREATE PROCEDURE get_categories_v1
+ (IN parameter_active BOOLEAN) BEGIN
+  SELECT
+    id,
+    `name`,
+    created,
+    updated,
+    active
+  FROM
+    category c
+  WHERE
+    c.active = parameter_active;
+  END;
+//
 
-END;
-
-/ / DELIMITER;
+DELIMITER ;
