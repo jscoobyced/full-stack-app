eae85a901935226f5a50029b27030988e3b99f0a
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-19 22:35:36 +0700

    Fix HTTP --> HTTPS

--- fe/src/config/constants.ts
+++ fe/src/config/constants.ts
@@ -3,7 +3,7 @@ export const COPYRIGHT = "Copyright"
 export const AUTHOR = "Cédric Rochefolle";
 export const RELEASE_YEAR = 2021;
 
-export const BACK_END_URL = `http://${process.env.REACT_APP_BACK_END_API_HOST}:${process.env.REACT_APP_BACK_END_API_PORT}/`;
+export const BACK_END_URL = `https://${process.env.REACT_APP_BACK_END_API_HOST}:${process.env.REACT_APP_BACK_END_API_PORT}/`;
 export const BACK_END_SERVICES_ENDPOINTS = {
   getIngredients: 'v1/ingredients',
 }
