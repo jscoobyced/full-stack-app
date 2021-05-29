dec5c40f9756b25f3a5f39bc1c935408a0faf3f7
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-29 21:36:15 +0700

    Fix version script

--- .github/docker-tag.sh
+++ .github/docker-tag.sh
@@ -3,6 +3,7 @@
 GHTAG=$(git name-rev --tags --name-only $(git rev-parse HEAD))
 GHBR=$(git rev-parse --abbrev-ref HEAD)
 DOCKER_TAG='dev'
+JSCWORKSPACE="$1"
 
 if [ "${GHTAG}" = "undefined" ]
 then
@@ -14,4 +15,5 @@ else
   DOCKER_TAG=${GHTAG}
 fi
 
-echo -e "DOCKER_TAG=${DOCKER_TAG}" >> $GITHUB_ENV
+echo -e "DOCKER_TAG=${DOCKER_TAG}" | tee -a $GITHUB_ENV
+sed -i "s/JSCVERSION/${DOCKER_TAG}/" "${JSCWORKSPACE}/fe/package.json"
