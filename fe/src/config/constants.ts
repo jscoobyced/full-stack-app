ccd9505b6c01bb046403d77afa6c25e45d0c5f69
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-06-03 19:20:30 +0700

    Save user info in database
    When login, save the user in database

--- fe/src/config/constants.ts
+++ fe/src/config/constants.ts
@@ -1,7 +1,10 @@
-export const TITLE = "Cooking for fun";
+export const TITLE = "Cooking is fun";
 export const COPYRIGHT = "Copyright"
 export const AUTHOR = "Cédric Rochefolle";
 export const RELEASE_YEAR = 2021;
+
+export const SIGNIN = 'Sign In';
+export const SIGNOUT = 'Sign Out';
 
 export const BACK_END_URL = `${process.env.REACT_APP_BACK_END_API_SCHEME}://${process.env.REACT_APP_BACK_END_API_HOST}:${process.env.REACT_APP_BACK_END_API_PORT}/`;
 export const BACK_END_SERVICES_ENDPOINTS = {
