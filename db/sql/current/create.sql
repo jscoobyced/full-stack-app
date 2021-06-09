446baf68d21c277747955148f97c8e2d203d95ab
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-06-09 08:17:51 +0700

    Integrate DB scripts and add more ingredients.

--- db/sql/current/create.sql
+++ db/sql/current/create.sql
@@ -68,3 +68,39 @@ CREATE TABLE `user` (
   active BOOLEAN,
   PRIMARY KEY (`uid`)
 );
+
+CREATE TABLE `recipe` (
+  id INT NOT NULL AUTO_INCREMENT,
+  recipe_name VARCHAR(128),
+  created DATETIME,
+  updated DATETIME,
+  active BOOLEAN,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE `selected_ingredient` (
+  id INT NOT NULL AUTO_INCREMENT,
+  recipe INT,
+  ingredient INT,
+  calorie INT,
+  serving DOUBLE,
+  created DATETIME,
+  updated DATETIME,
+  active BOOLEAN,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE
+  `selected_ingredient`
+ADD
+  CONSTRAINT FK_recipe_selected_ingredient FOREIGN KEY (recipe) REFERENCES `recipe`(id);
+
+ALTER TABLE
+  `selected_ingredient`
+ADD
+  CONSTRAINT FK_ingredient_selected_ingredient FOREIGN KEY (ingredient) REFERENCES `ingredient`(id);
+
+ALTER TABLE
+  `selected_ingredient`
+ADD
+  CONSTRAINT FK_calorie_selected_ingredient FOREIGN KEY (calorie) REFERENCES `calorie`(id);
---
+++ db/sql/update/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+  `calorie` (
+    id,
+    ingredient,
+    unit,
+    serving,
+    calories,
+    created,
+    updated,
+    active
+  )
+VALUES
+-- Salt
+(63, 17, 3, 48, 0, NOW(), NOW(), 1);
